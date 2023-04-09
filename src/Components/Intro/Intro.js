f1317a4203788231fd9a9b3e6223aedcc1a830cc
kadiri sunday <kadirisunday212@gmail.com>
2023-04-09 13:31:03 +0100

    response

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -3,10 +3,7 @@ import React from 'react'
 import './Intro.css'
 import Github from '../../img/github.png'
 import LinkedIn from '../../img/linkedin.png'
-// import Instagram from '../../img/instagram.png'
-import Vector1 from '../../img/Vector1.png'
-import Vector2 from '../../img/Vector2.png'
-// import boy from '../../img/boy.png'
+import kadiri from '../../img/kadiri.png'
 // import thumbup from '../../img/thumbup.png'
 // import Crown from '../../img/crown.png'
 // import glassimoji from '../../img/glassesimoji.png'
@@ -30,9 +27,9 @@ const Intro = () => {
         </div>
       </div>
       <div className='i-right'>
-          <img src={Vector1} alt=""/>
-          <img src={Vector2} alt=""/>
-          {/* <img src={boy} alt=""/> */}
+          {/* <img src={Vector1} alt=""/> */}
+          {/* <img src={Vector2} alt=""/> */}
+          <img src={kadiri} alt=""/>
           {/* <FloatingDiv image={Crown}/> */}
       </div>
     </div>
